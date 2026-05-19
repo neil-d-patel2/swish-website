71e3fbcb747c4f9e71a6c1351a28213fec967c4a
Neil <ebox381@gmail.com>
2026-05-18 21:49:40 -0400

    repush

--- convex/cleanup.ts
+++ convex/cleanup.ts
@@ -9,6 +9,7 @@ export const removeDeletedProjects = internalMutation({
       .withIndex("by_deletedAt", (q) => q.gt("deletedAt", 0))
       .collect();
 
+      
     for (const project of projects) {
       await ctx.scheduler.runAfter(0, internal.projects.deleteProjectCascade, {
         id: project._id,
