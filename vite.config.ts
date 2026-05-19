b1bc10cc1e41403b8a78f8efd806016817e7cd30
Neil <ebox381@gmail.com>
2026-05-18 21:52:13 -0400

    switched vite config to swish

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/issue-tracker/",
+  base: "/swish-website/",
   plugins: [tanstackRouter(), react(), tailwindcss()],
   resolve: {
     alias: {
