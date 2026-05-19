96694e850ce14c8fc4931e2899fbe1e98fc0ce21
Neil <ebox381@gmail.com>
2026-05-18 21:44:54 -0400

    sign in flow

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,7 +1,9 @@
+import { authTables } from "@convex-dev/auth/server";
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
+  ...authTables,
   projects: defineTable({
     name: v.string(),
     description: v.optional(v.string()),
