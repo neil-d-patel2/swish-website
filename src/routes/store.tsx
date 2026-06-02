eb634908256e0ef2db11dcdf4f91f5ffead43419
Neil <ebox381@gmail.com>
2026-06-02 01:49:39 -0400

    Fixed git error for cardcontent type

--- src/routes/store.tsx
+++ src/routes/store.tsx
@@ -6,7 +6,6 @@ import { useSupabaseAuth } from "@/hooks/use-supabase-auth";
 import { Button } from "@/components/ui/button";
 import {
   Card,
-  CardContent,
   CardHeader,
   CardTitle,
   CardDescription,
