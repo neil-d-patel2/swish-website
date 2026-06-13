e023dc1b66d932bb9a04948df4e4392a73258e9a
Neil <ebox381@gmail.com>
2026-06-13 04:12:07 -0400

    updated convex functions

--- src/components/create-project-dialog.tsx
+++ src/components/create-project-dialog.tsx
@@ -14,6 +14,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 
+
 function CreateProjectDialog() {
   const [open, setOpen] = useState(false);
   const [name, setName] = useState("");
